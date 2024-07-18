ce18c331590772a440e92165d9ccaea1bbef07c3
Nahidahmmed <nahidahmmed411@gmail.com>
2024-07-18 13:33:27 +0600

    single university is good to go

---
+++ src/Pages/Single_University/Single_University_Check.jsx
@@ -0,0 +1,15 @@
+const Single_University_Check = () => {
+    return (
+      <div className="flex flex-col md:flex-row items-center justify-between p-6 md:p-10 bg-gradient-to-tr from-[#ED1C24] to-[#FCEC21] rounded-[30px] md:w-full w-[95%] mx-auto ">
+        <p className="text-white text-lg md:text-2xl lg:text-3xl text-center md:text-left mb-4 md:mb-0">
+          Check all opportunities to study <br className="hidden md:block" /> at the University of Aberdeen
+        </p>
+        <button className="bg-white text-blue-500 font-semibold py-3 px-5 md:py-4 md:px-5 rounded-full shadow-md hover:bg-blue-100 transition duration-300 w-full md:w-auto">
+          Check Your Eligibility
+        </button>
+      </div>
+    );
+  };
+  
+  export default Single_University_Check;
+  
---
+++ src/Pages/Single_University/Single_Universty_Accordion.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { LuTriangle } from "react-icons/lu";
+
+const Single_Universty_Accordion = () => {
+  const [isOpen, setIsOpen] = useState(null);
+  const dataArr = [
+    {
+      title: "Overview",
+      description:
+        'To create an account, click on the "Sign Up" button and fill out the required information. Once done, you can enjoy the benefits of being a registered member.',
+    },
+    {
+      title: "History",
+      description:
+        "Our return policy allows you to return items within 30 days of purchase. Please visit our returns page for detailed instructions and to initiate a return.",
+    },
+    {
+      title: "Ranking & Achievements",
+      description:
+        "Yes, you can change your shipping address before your order is shipped. Go to your account settings and update the shipping information accordingly.",
+    },
+    {
+      title: "Courses",
+      description:
+        "We appreciate our loyal customers! Stay tuned for exclusive discounts, promotions, and special offers available to members of our loyalty program.",
+    },
+    {
+      title: "Departments and Faculties",
+      description:
+        "We appreciate our loyal customers! Stay tuned for exclusive discounts, promotions, and special offers available to members of our loyalty program.",
+    },
+    {
+      title: "Fees & Scholarships",
+      description:
+        "We appreciate our loyal customers! Stay tuned for exclusive discounts, promotions, and special offers available to members of our loyalty program.",
+    },
+    {
+      title: "Facilities & Services",
+      description:
+        "We appreciate our loyal customers! Stay tuned for exclusive discounts, promotions, and special offers available to members of our loyalty program.",
+    },
+    {
+      title: "Accommodation",
+      description:
+        "We appreciate our loyal customers! Stay tuned for exclusive discounts, promotions, and special offers available to members of our loyalty program.",
+    },
+    {
+      title: "Location",
+      description:
+        "We appreciate our loyal customers! Stay tuned for exclusive discounts, promotions, and special offers available to members of our loyalty program.",
+    },
+  ];
+
+  const toggle = (idx) => {
+    setIsOpen((prevIdx) => (prevIdx === idx ? null : idx));
+  };
+
+  return (
+    <div className="w-full h-screen rounded-lg px-5 py-10">
+      {dataArr.map((PerAccordion, idx) => (
+        <div key={idx} className="my-3">
+          <button
+            onClick={() => toggle(idx)}
+            className="flex w-full rounded-full bg-[#EEEEEE] items-center justify-between font-medium outline-none"
+          >
+            <span className="pl-5 py-3 text-lg md:text-2xl font-bold text-[#00399F]">{PerAccordion.title}</span>
+            <span className={`rounded-full text-lg md:text-xl px-8 py-4 ${isOpen === idx ? "bg-[#00399F]" : "bg-[#e1e1e1]"}`}>
+              <LuTriangle
+                className={`origin-center transform transition duration-200 ease-out rotate-90 text-[#00399F] ${
+                  isOpen === idx && "!rotate-180 text-white"
+                }`}
+              />
+            </span>
+          </button>
+          <div
+            className={`grid overflow-hidden transition-all duration-300 ease-in-out ${
+              isOpen === idx ? "grid-rows-[1fr] pb-1 pt-3 opacity-100" : "grid-rows-[0fr] opacity-0"
+            }`}
+          >
+            <div className="overflow-hidden pr-4 text-sm">
+              {PerAccordion.description}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Single_Universty_Accordion;
