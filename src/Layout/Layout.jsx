579d8b1d9881baf59e765304ed6030175bb94d08
sazzad46031 <git config --global user.email sazzadmgbs@gmail.com>
2024-07-30 00:14:06 +0600

    strange

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,9 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Shared/Footer/Footer";
 import Navbar from "../Shared/Navbar/Navbar";
-import Germany_Banner from "../Components/Germany_Banner/Germany_Banner";
-import Germany_Purpose from "../Components/Germany_Purpose/Germany_Purpose";
-import Germany_StudyInGermany from "../Components/Germany_StudyInGermany/Germany_StudyInGermany";
 
 
 
@@ -28,7 +25,6 @@ const Layout = () => {
             <Outlet/>
             
             <Footer/>
-            
         </div>
     );
 };
