e950b037c86c179f41bedb37d2181f59cf02aac0
sujit-mandal <sm.bright22@gmail.com>
2024-08-20 23:02:05 +0600

    UK Banner modified

--- src/Components/Study_Destination_Components/UK/UKBanner.jsx
+++ src/Components/Study_Destination_Components/UK/UKBanner.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import png from "../../../assets/UK/UK-Hero-Hover-1.png";
 import bgWhite from "../../../assets/UK/UK-Hero-Hover-BG-1.png";
 import bgBlue from "../../../assets/UK/UK-Hero-Hover-BG-2.png";
-const UKBanner = ({country}) => {
+const UKBanner = ({ country }) => {
   const [bgHover, setBgHover] = useState(0);
   return (
     <div
@@ -10,15 +10,15 @@ const UKBanner = ({country}) => {
       onMouseEnter={() => setBgHover(1)}
       onMouseLeave={() => setBgHover(0)}
     >
-      <div className="relative w-full mx-auto flex flex-col items-center justify-center ">
-        <div>
+      <div className="relative w-full mx-auto flex flex-col items-center justify-center">
+        <div className="w-full">
           {bgHover == 1 ? (
             <img className="w-full rounded-3xl" src={bgBlue} alt="" />
           ) : (
             <img className="w-full rounded-3xl" src={bgWhite} alt="" />
           )}
         </div>
-        <div className="  absolute bottom-0   max-w-7xl mx-auto">
+        <div className="absolute bottom-0 max-w-7xl mx-auto">
           <img className="rounded-3xl " src={png} alt="" />
         </div>
         <div className="absolute z-10 top-[10%] flex flex-col items-center justify-center">
