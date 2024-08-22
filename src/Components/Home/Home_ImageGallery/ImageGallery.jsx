4689eb6d4c3fd3ec662bab4a18a6f090bd331f68
riyaz <riyaz3582@gmail.com>
2024-08-22 07:19:56 +0600

    fixed marq

--- src/Components/Home/Home_ImageGallery/ImageGallery.jsx
+++ src/Components/Home/Home_ImageGallery/ImageGallery.jsx
@@ -5,10 +5,8 @@ import { motion } from "framer-motion"
 
 function App() {
 
-
-
-
-
+const [modal, setModal] = useState({})
+console.log(Object.keys(modal).length)
 
   return (
     <div className="main flex flex-col items-center align overflow-hidden">
@@ -17,8 +15,10 @@ function App() {
       <Marquee pauseOnHover={true} autoFill={true} >
         {
           data.map((data, index) => (
-            <motion.div initial={{width:'172px'}} whileHover={{width:'368px'}} transition={{duration:.3, }} key={index}
-              onClick={() => document.getElementById('my_modal_1').showModal()}
+            <motion.div initial={{ width: '172px' }} whileHover={{ width: '368px' }} transition={{ duration: .3, }} key={data.id}
+              onClick={() => {
+                setModal(data.id)
+                document.getElementById('my_modal_5').showModal()}}
               className="img1 w-[172px] hover:w-[368px] h-[350px]   rounded-[20px] mr-[10px] cursor-pointer   bg-white bg-opacity-30   grayscale hover:filter-none relative overflow-hidden group   mt-[50px] ">
               <div style={{ background: 'linear-gradient(93deg, rgba(37,180,186,1) 0%, rgba(222,243,135,1) 100%)' }} className="hidden absolute  bottom-0 special p-[10px]  rounded-b-[15px] group-hover:block">
                 <h1 className="text-white font-[500] text-[18px] leading-7">{data.title}</h1>
@@ -33,8 +33,10 @@ function App() {
       <Marquee pauseOnHover={true} direction="right" autoFill={true}  >
         {
           data.map((data, index) => (
-            <motion.div initial={{width:'172px'}} whileHover={{width:'368px'}} transition={{duration:.3, }} key={index}
-              onClick={() => document.getElementById('my_modal_1').showModal()}
+            <motion.div initial={{ width: '172px' }} whileHover={{ width: '368px' }} transition={{ duration: .3, }} key={data.id}
+              onClick={() => {
+                setModal(data)
+                document.getElementById('my_modal_5').showModal()}}
               className="img1 w-[172px] hover:w-[368px] h-[350px]   rounded-[20px] mr-[10px] cursor-pointer   bg-white bg-opacity-30   grayscale hover:filter-none relative overflow-hidden group   mt-[50px] ">
               <div style={{ background: 'linear-gradient(93deg, rgba(37,180,186,1) 0%, rgba(222,243,135,1) 100%)' }} className="hidden absolute  bottom-0 special p-[10px]  rounded-b-[15px] group-hover:block">
                 <h1 className="text-white font-[500] text-[18px] leading-7">{data.title}</h1>
@@ -46,18 +48,24 @@ function App() {
         }
       </Marquee>
 
-      <dialog id="my_modal_1" className="modal">
+      {Object.keys(modal).length &&  <dialog id={modal.id} className="modal">
         <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click the button below to close</p>
-          <div className="modal-action">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
-            </form>
+          <form method="dialog">
+            <button className="btn btn-md btn-circle text-lg text-blue-600 absolute z-10 right-2 top-2">✕</button>
+          </form>
+          <div className='relative'>
+            <img src={modal.url} className='w-[380px] h-auto' alt="" />
+            <div className='modal-bg pb-3'>
+              <h2 className='poppins-medium text-[18px] pt-4 pl-4 text-white'>Exciting Announcement!</h2>
+              <p className='poppins-regular text-[14px] pl-4 text-white'>We are delighted to invite you to a special event on 17th January 2024, from 12:00 pm to 02:00 pm at the Shabuj Global Education Chittagong Branch. Meet the University of Portsmouth representative, Mr. Md. Nuruzzaman.</p>
+            </div>
+
           </div>
         </div>
-      </dialog>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>}
 
 
 
