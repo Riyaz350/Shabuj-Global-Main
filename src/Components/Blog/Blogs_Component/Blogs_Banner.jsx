90708901b7b3498245585d96514afe062289c7cb
zannat20040 <shanzidadisha1234@gmail.com>
2024-09-20 14:15:15 +0600

    Blog page edit

--- src/Components/Blog/Blogs_Component/Blogs_Banner.jsx
+++ src/Components/Blog/Blogs_Component/Blogs_Banner.jsx
@@ -20,7 +20,7 @@ const Blogs_Banner = () => {
             </div>
             <div className='absolute w-full  top-1/4 lg:top-1/2'>
                 <div className={`${width >= 1024 & width <= 1300 ? 'max-w-4xl ml-auto' : width > 1300 ? 'max-w-6xl mx-auto' : 'max-w-4xl'}  flex flex-col lg:flex-row items-center   relative`}>
-                    <motion.div initial={{x:'-200px'}} animate={{x:0}} transition={{duration:.5}} className={`${width >= 1024 & width <= 1300 ? 'max-w-xl -left-28' : width > 1300 ? 'max-w-2xl' : 'max-w-xs'}  lg:absolute z-20   overflow-hidden `}>
+                    <motion.div initial={{x:'-200px'}} animate={{x:0}} transition={{duration:.5}} className={`${width >= 1024 & width <= 1300 ? 'max-w-xl -left-28' : width > 1300 ? 'max-w-2xl' : 'max-w-xs'}  lg:absolute z-20   overflow-hidden  px-5`}>
                         <div className='  bg-white  shadow-lg px-5 py-10 rounded-2xl'>
                             <div className='text-white font-light mb-5 bg-[#ff8156] w-fit px-2 rounded-md'>UK University</div>
                             <h1 className='text-2xl lg:text-5xl mb-2 lg:mb-5 font-semibold'>Choosing the Right UK University and Course: Factor to Consider</h1>
@@ -30,7 +30,7 @@ const Blogs_Banner = () => {
                             </Link>
                         </div>
                     </motion.div>
-                    <motion.div initial={{x:'200px'}} animate={{x:0}} transition={{duration:.5}} className='lg:absolute hidden lg:flex   justify-end z-10 lg:px-5 -right-0'>
+                    <motion.div initial={{x:'200px'}} animate={{x:0}} transition={{duration:.5}} className='lg:absolute hidden lg:flex   justify-end z-10 px-5 -right-0'>
                         <img className=' w-10/12 mx-auto lg:mx-0' src='https://i.ibb.co.com/sH5hvdY/Rectangle-190.png' alt="" />
                     </motion.div>
 
