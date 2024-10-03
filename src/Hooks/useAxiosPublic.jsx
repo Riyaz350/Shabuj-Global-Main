c0d7eff0ceea0f3e7733ebde6037abef0040d61b
riyaz <riyaz3582@gmail.com>
2024-10-03 19:48:39 +0600

    added api

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
     // baseURL:'https://nodebot-umber.vercel.app'
-    baseURL:'http://api.studyuk.today'
+    baseURL:'https://api.studyuk.today'
 })
 const useAxiosPublic = () => {
     return axiosPublic
